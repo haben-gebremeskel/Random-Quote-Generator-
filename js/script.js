dba520a5fe4ff7714d0dbaaed24caaddc6db733d
ingeniousarchitect <60585776+ingeniousarchitect@users.noreply.github.com>
2020-02-06 10:47:42 -0500

    Added project starter files

---
+++ js/script.js
@@ -0,0 +1,109 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+var quotes = [
+  {
+    quote: 'The very word "secrecy" is repugnant in a free and open society; and we are as a people inherently and historically opposed to secret societies, to secret oaths and to secret proceedings.',
+    source: 'President John F. Kennedy',
+    citation: 'Address before the American Newspaper Publishers Association',
+    year: 1961
+  },
+  {
+    quote: 'If I’m going to fall, I don’t want to fall back on anything, except my faith. I want to fall… forward. At least I figure that way I’ll see what I’m about to hit. Fall forward.',
+    source: 'Denzel Washington',
+    citation: 'Commencement speech at the University of Pennsylvania',
+    year: 2011
+  },
+  {
+    quote: 'You have great wisdom, Anakin. Know the power of the dark side. The power to save Padme.',
+    source: 'Chancellor Palpatine',
+    citation: 'Star Wars Episode III: Revenge of the Sith',
+    year: 2005
+  },
+  {
+    quote: 'One day, you might look up and see me playing the game at 50. Don\'t laugh. Never say never, because limits, like fears, are often just an illusion.',
+    source: 'Michael Jordan',
+    citation: 'Basketball Hall of Fame induction speech',
+    year: 2009
+  },
+  {
+    quote: 'No independent person was born and just grew up independent. You worked and you learned teamwork and cooperation and unity and struggle and then you became independent. We have to teach that and instill that.',
+    source: 'Tupac Shakur',
+    citation: 'MTV Interview',
+    year: 1992
+  },
+  {
+    quote: 'Every negative is a positive. The bad things that happen to me, I somehow make them good. That means you can’t do anything to hurt me.',
+    source: '50 Cent',
+    citation: '50th Law of Power',
+    year: 2009
+  },
+  {
+    quote: 'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma – which is living with the results of other people\'s thinking.',
+    source: 'Stve Jobs',
+    citation: 'Stanford University commencement speech',
+    year: 2005
+  },
+  {
+    quote: 'I’ve lived in prison a long time now. I’ve met a lot of men who were motivated to commit violence just like me. And without exception, every one of them was deeply involved in pornography. Without question, without exception, deeply influenced and consumed by addiction to pornography.',
+    source: 'Ted Bundy',
+    citation: 'Interview at the Florida State Prison',
+    year: 1989
+  },
+  {
+    quote: 'I think it is the obligation of the people that have created and perpetuate and benefit from a system of oppression to be the ones that dismantle it.',
+    source: 'Joaquin Phoenix',
+    citation: 'British Academy of Film and Television ceremony award acceptance speech',
+    year: 2020
+  },
+  {
+    quote: 'Because if you are humble, if you make people realize that you are no threat to them, then people will embrace you',
+    source: 'Nelson Mandela',
+    citation: 'Oprah Winfrey Show Interview',
+    year: 2000
+  },
+  {
+    quote: 'There is a difference between knowing the path and walking the path.',
+    source: 'Morpheus',
+    citation: 'The Matrix',
+    year: 1999
+  },
+  {
+    quote: 'Many that live deserve death. And some that die deserve life. Can you give it to them Frodo? Do not be too eager to deal out death in judgement. For even the very wise cannot see all ends.',
+    source: 'Gandalf the Grey',
+    citation: 'Lord of the Rings: The Fellowship of the Ring',
+    year: 2001
+  }
+];
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+function getRandomQuote() {
+  
+}
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
